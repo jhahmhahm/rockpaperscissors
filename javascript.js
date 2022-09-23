2fb52ae7ac103bad380978ff7eae37d37615d5e0
jhahmhahm <jonhahm@gmail.com>
2022-09-23 00:29:50 -0400

    playerchoice fixed

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 // an array consisting of Rock, Paper and Scissors 
 let arrayrps = ['Rock', 'Paper', 'Scissors'];
 let computerchoice;
-let playerchoice
+let playerchoice;
 
 function getComputerChoice(){
     let index = Math.floor(Math.random()*3); // generates a number between 0 and 2
@@ -11,27 +11,24 @@ function getComputerChoice(){
 }
 
 function getPlayerChoice(){
-    let playerchoice = prompt("Rock, Paper, or Scissors?"); // asks the user to pick rock, paper, or scissors
+    playerchoice = prompt("Rock, Paper, or Scissors?"); // asks the user to pick rock, paper, or scissors
     playerchoice = playerchoice.toUpperCase(); // changes our userinput to all caps - makes it case insensitve
     return playerchoice;
 }
 
 
 function playround(x, y){ //plays a round of rock paper scissors 
-    let playerchoice = getPlayerChoice(); //calls upon the playerchoice function so the player can make their choice
-    let computerchoice = getComputerChoice(); //calls upon the computerchoice function so the computer can make their choice 
-    x = playerchoice; 
-    y = computerchoice;
+    x = getPlayerChoice(); //calls upon the playerchoice function so the player can make their choice
+    y = getComputerChoice(); //calls upon the computerchoice function so the computer can make their choice 
     
-    if(playerchoice === "PAPER" && computerchoice === "ROCK"){
-        alert("You win!");
-    }else if(playerchoice === "ROCK" && computerchoice === "SCISSORS"){
-        alert("You win!");
-    } else if(playerchoice === "SCISSORS" && computerchoice === "PAPER"){
-        alert("You win!");
-    } else if(playerchoice === computerchoice){
+    if(playerchoice == computerchoice){
         alert("It's a tie!");
-    }else {
+    } else if ((playerchoice === "PAPER" && computerchoice === "ROCK")||
+               (playerchoice === "ROCK" && computerchoice === "SCISSORS")||
+               (playerchoice === "SCISSORS" && computerchoice === "PAPER")){
+                alert("You win!");
+    } 
+    else {
         alert("You lose. Sorry!");
     }
 }
